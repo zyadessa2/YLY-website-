156bb22a840834a5a269dae88b50ee1f59c17e7a
ziad <helmyzez@gmail.com>
2026-05-01 23:32:10 +0300

    fix: remove unnecessary class from news detail page layout

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -92,7 +92,7 @@ export default async function NewsDetailPage({ params }: NewsPageProps) {
         />
 
         {/* Main Content */}
-        <div className=" mx-auto px-4 py-8">
+        <div className=" mx-auto  py-8">
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
             {/* News Content */}
             <div className="lg:col-span-2">
